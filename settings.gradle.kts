a1b386ec3af34e4785b2f6cd370515f6a481a47c
Mark-Masomi <66164227+Mark-Masomi@users.noreply.github.com>
2025-03-20 15:20:14 +0100

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+rootProject.name = "ecommerce-microservices"
+
+include(
+    "service-registry",
+    "api-gateway",
+    "product-service",
+    "order-service",
+    "user-service",
+    "inventory-service"
+)
